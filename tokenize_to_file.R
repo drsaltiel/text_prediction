c7a71b59e2aaa450000c37a6f2119cdf82a8ca48
Daniel <drsaltiel@gmail.com>
2014-11-22 18:30:51 -0800

    added to script

--- tokenize_to_file.R
+++ tokenize_to_file.R
@@ -9,18 +9,26 @@ paths = c('~/coursera/data_science_coursera/capstone/final/en_US/en_US.blogs.txt
           '~/coursera/data_science_coursera/capstone/final/en_US/en_US.twitter.txt')
 paths <- c(blogs, news, twitter)
 
-
+path <- 'all' #for naming file if only one specific source
+n_grams = 3
 fraction = 0.01
 
 #need this to write tokens as a list
 #token, appearances
 for (path in paths){
     con <- file(path, "r") 
-    line <- readLines(con, 1)
     tokens <- vector(mode="list")
-    if (runif(1)<fraction){
-        #here i need to check if line is already there
-        #if it is append by one, if not create new element in list
+    line <- readLines(con, 1)
+    while (LINE IS NOT LAST LINE){
+        if (runif(1)<fraction){
+            line <- strsplit(line[1], split = '[.!?]')
+            for (sent in line){
+                #here i need to check if gram is already there
+                #if it is append by one, if not create new element in list
+                gram<-tolower(NGramTokenizer(sent, Weka_control(min = n_grams, max = n_grams)))
+            }
+        }
+        line<-readLines(con,1)
     }
     
     tokens<-lapply(tokens,remove_sparse)
@@ -28,7 +36,8 @@ for (path in paths){
     #remove NA elements
     tokens <- tokens[!is.na(tokens)]
     
-    write(tokens,file="tokens.txt",append=TRUE)
+    filename<-paste('path','tokens',n_grams,'.txt',sep='')
+    write(tokens,file=filename)
     close(con)
 }
 
