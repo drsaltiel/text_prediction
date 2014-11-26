6755c4d5ae4ed7a5c3704acc92535f6309b595bb
Daniel <drsaltiel@gmail.com>
2014-11-25 20:43:25 -0800

    added function to suggest from tokens with frequencies

--- predictor.R
+++ predictor.R
@@ -1,5 +1,5 @@
 
-suggest_from_tokens<-function(phrase, tokens){
+suggest_from_tokens_list<-function(phrase, tokens){
     #tokens_list<-c(tokens3, tokens2)
 
     t<-grep(phrase, tokens)
@@ -19,4 +19,26 @@ suggest_from_tokens<-function(phrase, tokens){
         return(NULL)
     }
 
+}
+
+suggest_from_tokens_freq<-function(phrase, tokens){
+    #tokens_list<-c(tokens3, tokens2)
+    
+    t<-grep(phrase, names(tokens))
+    candidates <- c()
+    
+    #for tokens with matching phrase, add next word to candidates
+    for (i in t){
+        candidates <- c(candidates, tail(strsplit(names(tokens[i+1]), split=' ')[[1]],1))
+    }
+    if (length(candidates) != 0){
+        top <- sort(table(candidates), decreasing=TRUE)
+        suggestion <- names(top[1])
+        #num <- max(top)
+        return(suggestion)
+    }
+    else{
+        return(NULL)
+    }
+    
 }
