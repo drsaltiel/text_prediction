23e563dd639df8d2ac20f578244dfab3d11c25bd
Daniel <drsaltiel@gmail.com>
2014-11-27 00:03:59 -0800

    commented and cleaned up

--- tokenize_to_file.R
+++ tokenize_to_file.R
@@ -1,17 +1,20 @@
 #script takes text files input, tokenizes, then writes tokenization to file
 
+n_grams = 3
+fraction = 0.001
+p <- 'all' #for naming file if only one specific source
+
 library(tm)
 library(RWeka)
 
 options(mc.cores=1)
-#paths = c('~/coursera/data_science_coursera/capstone/final/en_US/en_US.blogs.txt',
-#          '~/coursera/data_science_coursera/capstone/final/en_US/en_US.news.txt',
-#          '~/coursera/data_science_coursera/capstone/final/en_US/en_US.twitter.txt')
-#paths <- c(blogs, news, twitter)
-paths<-c('~/coursera/data_science_coursera/capstone/final/en_US/en_US.twitter.txt')
-p <- 'twitter' #for naming file if only one specific source
-n_grams = 3
-fraction = 0.001
+paths = c('~/coursera/data_science_coursera/capstone/final/en_US/en_US.blogs.txt',
+          '~/coursera/data_science_coursera/capstone/final/en_US/en_US.news.txt',
+          '~/coursera/data_science_coursera/capstone/final/en_US/en_US.twitter.txt')
+
+#paths<-c('~/coursera/data_science_coursera/capstone/final/en_US/en_US.twitter.txt')
+
+
 tokens <- vector(mode="list")
 #write tokens as a list
 #token, appearances
@@ -24,6 +27,8 @@ for (path in paths){
             for (sent in line){
                 #check if gram is already there
                 #if it is append by one, if not create new element in list
+                #SHOULD I CHANGE TO FIT PUNCTUATION, ETC????
+                #???????????????????????????????????
                 grams<-tolower(NGramTokenizer(sent, Weka_control(min = n_grams, max = n_grams)))
                 for(gram in grams){
                     if (gram %in% names(tokens)) {
@@ -47,9 +52,9 @@ tokens <- tokens[!is.na(tokens)]
 
 filename<-paste('~/coursera/data_science_coursera/capstone/final/', p,'tokens',n_grams,'.txt',sep='')
 
-output_list(tokens)
+output_list(tokens) #prints to file .txt
+save(tokens, file="processed.RData") # saves to RData
 
-#lapply(flist, write, filename, append=TRUE)
 
 
 
@@ -62,6 +67,7 @@ remove_sparse<-function(data, min_n = 2){
 }
 
 output_list <- function(x){ 
+    #prints to file
     nams=names(x) 
     for (i in seq_along(x) ) cat(nams[i],  x[[i]], "\n", file=filename, append=TRUE)
 }
