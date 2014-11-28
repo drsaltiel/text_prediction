3049ec421ae306925b946eeec6c10e7705987f28
Daniel <drsaltiel@gmail.com>
2014-11-27 20:22:59 -0800

    fixed suggestion function bugs, but gives new bugs i dont understand.  needs to be fixed

--- server.R
+++ server.R
@@ -6,24 +6,36 @@ options(shiny.maxRequestSize=30*1024^2)
 
 load('processed.RData')
 
+
+#GIVES WEIRD ERROR, NEED TO FIGURE OUT AND FIX
 suggest_from_tokens_freq<-function(phrase, tokens){
     if (phrase==''){return(NULL)}
     #takes last n words for prediction
-    phrase<- paste(tail(strsplit(phrase, split = ' ')[[1]], 3), collapse = ' ')
+    
+    phrase<- paste(tail(strsplit(phrase, split = ' ')[[1]], 2), collapse = ' ')
     
     t<-grep(phrase, names(tokens))
     candidates <- c()
     
     #for tokens with matching phrase, add next word to candidates
     for (i in t){
-        #adds next word after match to candidates
-        candidates <- c(candidates, tail(strsplit(names(tokens[i+1]), split=' ')[[1]],1))
+        #using bad hack of matching first word means beginning of phrase matches
+        #SHOULD BE FIXED
+        split_t<-strsplit(names(tokens[i]), split=' ')[[1]]
+        if (strsplit(phrase, split=' ')[[1]][1] == split_t[1]){
+            if (is.null(candidates[[tail(split_t,1)]])){
+                candidates[[tail(split_t,1)]]<-tokens[[i]]
+            }
+            else{
+                candidates[[tail(split_t,1)]]<-
+                    candidates[[tail(split_t,1)]]+tokens[[i]]
+            }
+        }
     }
     if (length(candidates) != 0){
-        #finds top match and returns ('match word', # of times this word follows)
-        top <- sort(table(candidates), decreasing=TRUE)
-        suggestion <- names(top[1])
-        num <- max(top)
+        #top <- sort(table(candidates), decreasing=TRUE)
+        suggestion <- names(which.max(candidates))
+        num <- candidates[[suggestion]]
         return(c(suggestion, num))
     }
     else{
